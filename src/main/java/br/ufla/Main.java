e83ba13a33a2bb95794f2256ba976b66d2f5ecc2
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2023-02-23 18:07:40 -0300

    WIP configuração de pom.

--- src/main/java/br/ufla/Main.java
+++ src/main/java/br/ufla/Main.java
@@ -2,26 +2,25 @@ package br.ufla;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void  main (String[] args){
-        String filename = "/home/luiz/MEGA/UFLA/22-02/compiladores2022-02/Fibonacci.txt";
-        try{
-            CharStream input = CharStreams.fromFileName(filename);
-            MinhaGramaticaLexer lexer = new MinhaGramaticaLexer(input);
-            Token token;
-            while (!lexer._hitEOF){
-                token = lexer.nextToken();
-                System.out.println("Token: "+ token.toString());
-                System.out.print("\tLexema: "+ token.getText());
-                System.out.println("\tClasse: "+lexer.getVocabulary().getDisplayName(token.getType()));
-            }
+    public static void main(String[] args) {
+        try {
+            CharStream entrada = CharStreams.fromFileName(args[0]);
+            MinhaGramaticaLexer lexer = new MinhaGramaticaLexer(entrada);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            MinhaGramaticaParser parser = new MinhaGramaticaParser(tokens);
 
-        } catch (IOException e){
+            ParseTree ast = parser.inicio();
+
+            System.out.println(ast.toStringTree());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
