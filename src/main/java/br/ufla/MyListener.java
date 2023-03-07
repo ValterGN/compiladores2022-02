32551639d7ec8058f58301bcd0a569b9a5995ff5
Valter Granato Neto <valter.neto@estudante.ufla.br>
2023-03-07 16:06:46 -0300

    Considerando escopo

--- src/main/java/br/ufla/MyListener.java
+++ src/main/java/br/ufla/MyListener.java
@@ -4,15 +4,41 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MyListener implements MinhaGramaticaListener{
-    Map<String, String> variaveis = new HashMap<>();
+    List<HashMap<String, String>> escopos = new ArrayList<>();
+
+    private void addVariavel(String id, String tipo){
+        escopos.get(escopos.size()-1).put(id, tipo);
+    }
+
+    private boolean variavelExiste(String id){
+        for (HashMap<String, String> hashMap : escopos) {
+            if(hashMap.containsKey(id)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String tipoDaVariavel(String id){
+        for (HashMap<String, String> hashMap : escopos) {
+            if(hashMap.containsKey(id)){
+                return hashMap.get(id);
+            }
+        }
+
+        return null;
+    }
 
     @Override
     public void enterBloco(MinhaGramaticaParser.BlocoContext ctx) {
-
+        escopos.add(new HashMap<String, String>());
     }
 
     @Override
@@ -82,8 +108,8 @@ public class MyListener implements MinhaGramaticaListener{
 
     @Override
     public void enterDeclaracaoVar(MinhaGramaticaParser.DeclaracaoVarContext ctx) {
-        if(!variaveis.containsKey(ctx.ID().getText())){
-            variaveis.put(ctx.ID().getText(), ctx.Tipo().getText());
+        if(!variavelExiste(ctx.ID().getText())){
+            addVariavel(ctx.ID().getText(), ctx.Tipo().getText());
         } else {
             System.out.println("Variável " + ctx.ID().getText() + " já foi declarada!!!");
         }
@@ -96,32 +122,32 @@ public class MyListener implements MinhaGramaticaListener{
 
     @Override
     public void enterCondicional(MinhaGramaticaParser.CondicionalContext ctx) {
-
+        escopos.add(new HashMap<String, String>());
     }
 
     @Override
     public void exitCondicional(MinhaGramaticaParser.CondicionalContext ctx) {
-
+        escopos.remove(escopos.size()-1);
     }
 
     @Override
     public void enterCondicionalSenao(MinhaGramaticaParser.CondicionalSenaoContext ctx) {
-
+        escopos.add(new HashMap<String, String>());
     }
 
     @Override
     public void exitCondicionalSenao(MinhaGramaticaParser.CondicionalSenaoContext ctx) {
-
+        escopos.remove(escopos.size()-1);
     }
 
     @Override
     public void enterRepeticao(MinhaGramaticaParser.RepeticaoContext ctx) {
-
+        escopos.add(new HashMap<String, String>());
     }
 
     @Override
     public void exitRepeticao(MinhaGramaticaParser.RepeticaoContext ctx) {
-
+        escopos.remove(escopos.size()-1);
     }
 
     @Override
@@ -176,6 +202,9 @@ public class MyListener implements MinhaGramaticaListener{
 
     @Override
     public void enterAtribuicao(MinhaGramaticaParser.AtribuicaoContext ctx) {
+        if(!variavelExiste(ctx.ID().getText())){
+            System.out.println("Variável " + ctx.ID().getText() + " não foi declarada");
+        }
 
     }
 
